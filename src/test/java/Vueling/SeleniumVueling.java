14956d4e0558fe2187b1212bd99f615a5308a492
Mario López Gil <mlopez@hiberus.com>
2023-04-27 12:40:24 +0200

    cambios

--- src/test/java/Vueling/SeleniumVueling.java
+++ src/test/java/Vueling/SeleniumVueling.java
@@ -1,14 +1,12 @@
 package Vueling;
 
+
+
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
